dcee382b591a3f1b7e2b36023a4592591cffe894
SasaStjepanovic <stjepanovics80@yahoo.com>
2024-09-07 13:59:55 +0200

    Runner class and xml file are added

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,22 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = "steps",
+        tags = "@ipon",
+        plugin = {"io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"}
+)
+
+public class Runner extends AbstractTestNGCucumberTests {
+
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
